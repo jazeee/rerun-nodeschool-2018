2e94488fd3322b30a0c9b22cb754720330460643
Jaz Singh <jsingh@grailbio.com>
2018-01-22 16:05:05 -0800

    04-proptypes

--- learn-you-react/views/index.jsx
+++ learn-you-react/views/index.jsx
@@ -20,6 +20,7 @@ class TodoList extends React.Component {
 					<tbody>
 						<Todo title="Shopping">Milk</Todo>
 						<Todo title="Hair cut">13:00</Todo>
+						<Todo title="Learn React">15:00</Todo>
 					</tbody>
 				</table>
 			</div>
@@ -39,6 +40,9 @@ const Todo = (props) => {
 		</tr>
 	);
 }
+Todo.propTypes = {
+	title: React.PropTypes.string.isRequired
+};
 
 class TodoForm extends React.Component {
 	render() {
